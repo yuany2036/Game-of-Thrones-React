2c111da4df226fa4a8a81cfac8fafefa40d7e5b3
Yu-An <yuany2036@gmail.com>
2023-01-04 15:32:32 +0100

    finished navbar

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,3 +1,25 @@
-const NavBar = () => {};
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.scss";
+
+const NavBar = () => {
+  return (
+    <ul className={styles.navbar}>
+      <li className={styles.logo}>
+        <NavLink to="/">
+          <div className={styles.img}></div>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/characters">Characters</NavLink>
+      </li>
+      <li>
+        <NavLink to="/houses">Houses</NavLink>
+      </li>
+    </ul>
+  );
+};
 
 export default NavBar;
